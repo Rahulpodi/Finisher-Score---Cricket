fad18021b0ebee2d2bedf5d8823f4b9f8922936f
Rahulpodi <rahulpodi@gmail.com>
2017-06-03 14:10:18 +0530

    Final Code

--- Codes/Finisher_Score.R
+++ Codes/Finisher_Score.R
@@ -1,53 +1,19 @@
 library(stringi)
 library(stringr)
-
 # Reading player batting data set
-
 player_dataset<-read.csv("E:/Cricinfo/cricinfo_batting.csv",stringsAsFactors = FALSE)
-
 # Reading match results data set
-
 match_result_dataset<-read.csv("E:/Cricinfo/cricinfo_match_results.csv",stringsAsFactors = FALSE)
-
 ##########################################################################################################
-
 # Cleaning player dataset
-
 # Changing column names
-
 colnames(player_dataset)<-c("Player.ID","Player.Name","Fours","Sixes","BF",
                             "Blank","Dismissal","Ground","Inns","Mins","ODI.number",
                             "Opposition","POS","Runs","SR","Start.Date")
-
-# Explaining the data set
-
-# Player.ID = ID assigned by cricinfo which was used for scrapping
-# Player.Name = Name of the player ex. Sachin Tendulkar
-# 4s = No of fours
-# 6s = No. of sixes
-# BF = no. of balls faced
-# Dismissal = mode of dismissal i..e, caught, bowled
-# Ground = the ground in which the corresponding ODI was played
-# Inns = first or second innings in a ODI
-# Mins = time spent on the field by the player
-# ODI.number = counting the number of ODIs
-# Start.date = the date on which the match was played
-# opposition = Opposition country
-# runs = total runs scored with * denoting not-out
-# score = Runs scored
-# SR = strike rate
-# POS = batting poisition
-
-# Cleaning the data set
-
 player_dataset_v1<-player_dataset[,-6] # Removing column called blank
-
 # Removing a row for each of the player which had blanks - due to scrapping
-
 player_dataset_v1<-player_dataset_v1[-which(player_dataset_v1$ODI.number==""),]
-
 # Making all the - to 0, actually in all the columns
-
 player_dataset_v1[which(player_dataset_v1$Fours=="-"),"Fours"]<-0
 player_dataset_v1[which(player_dataset_v1$Sixes=="-"),"Sixes"]<-0
 player_dataset_v1[which(player_dataset_v1$BF=="-"),"BF"]<-0
@@ -55,123 +21,61 @@ player_dataset_v1[which(player_dataset_v1$Dismissal=="-"),"Dismissal"]<-"No info
 player_dataset_v1[which(player_dataset_v1$Mins=="-"),"Mins"]<-0
 player_dataset_v1[which(player_dataset_v1$POS=="-"),"POS"]<-0
 player_dataset_v1[which(player_dataset_v1$SR=="-"),"SR"]<-0
-
 # Unfortunately even "Inns" has a "-" - may be the match was abandoned
-
 player_dataset_v1[which(player_dataset_v1$Inns=="-"),"Inns"]<-0
-
 # Converting into date format for further operations
-
 player_dataset_v1$Start.Date<-as.Date(player_dataset_v1$Start.Date,format = "%d %b %Y")
-
 # Removing "v" in opposition
-
 player_dataset_v1$Opposition<-data.frame(t(data.frame(lapply(player_dataset_v1$Opposition,function(x) strsplit(x," ")[[1]][2]))))[,1]
-
 # Obtianing not-out and out status
-
 player_dataset_v1[,"Carried.Bat"]<-data.frame(as.character(lapply(strsplit(player_dataset_v1$Runs, ''), function(x) which(x == '*'))))[,1] 
-
 # Assigning the same
-
 player_dataset_v1[which(player_dataset_v1$Carried.Bat=="integer(0)"),"Carried.Bat.Stauts"]<-"Out/DNB/TDNB"
 player_dataset_v1[which(player_dataset_v1$Carried.Bat!="integer(0)"),"Carried.Bat.Stauts"]<-"Not-out"
-
 # Removing the DNB,TDNB,sub,absent from the runs
-
 player_dataset_v1<-player_dataset_v1[-which(player_dataset_v1$Runs=="DNB" | player_dataset_v1$Runs=="TDNB"| player_dataset_v1$Runs=="sub" | player_dataset_v1$Runs=="absent"),]
-
 # Since not-out and out is determined, now the runs are made only numeric
-
 player_dataset_v1[,"Runs.Changing"]<-data.frame(t(data.frame(lapply(player_dataset_v1$Runs,function(x) str_replace_all(x,"[[:punct:]]","")))))
-
 # Creating player and ODI number concatenate to vlookup between data frames
-
 player_dataset_v1[,"Concatenate"]<-paste0(player_dataset_v1[,"Player.Name"],player_dataset_v1[,"ODI.number"])
-
 ####################################################################################################################
-
 # Similarly, cleaning match results dataset
-
-# Explaining the data set
-
-# Player.ID = ID assigned by cricinfo which was used for scrapping
-# Player.Name = Name of the player ex. Sachin Tendulkar
-# BR = Balls remaining
-# Bat = Similar to "Inns" above
-# Ground = similar to the above
-# Margin = margin by which the team won..by chasing it would be in wickets defending it would be runs
-# ODI.number = counting the number of ODIs
-# Start.date = the date on which the match was played
-# opposition = Opposition country
-# Result = most importantly did the team win or not
-# Toss = toss lost or won
-
-# Cleaning the data set
-
 match_result_dataset_v1<-match_result_dataset[,-5] # Removing column called blank
-
 # Removing a row for each of the player which had blanks - due to scrapping
-
 match_result_dataset_v1<-match_result_dataset_v1[-which(match_result_dataset_v1$ODI.Number==""),]
-
 # Making all the - to 0 actually or NA's to 0 in all the columns - this doesnt affect much since
 # they ae all from the 1st innings 99.9% of the times
-
 match_result_dataset_v1[which(match_result_dataset_v1$Margin=="-"),"Margin"]<-"999"
 match_result_dataset_v1[which(match_result_dataset_v1$Margin==""),"Margin"]<-"999"
 match_result_dataset_v1[which(is.na(match_result_dataset_v1$BR)),"BR"]=999
-
 # Again, concatenating player name and ODI number
-
 match_result_dataset_v1[,"Concatenate"]<-paste0(match_result_dataset_v1[,"Player.Name"],match_result_dataset_v1[,"ODI.Number"])
-
 ####################################################################################################################
-
 # Final dataset matching the two datasets
-
 final_dataset<-player_dataset_v1[,c(1,2,10,17,18,19)]
-
 final_dataset[,7:10]<-match_result_dataset_v1[match(final_dataset[,6],match_result_dataset_v1[,12]),c(3,4,6,9)]
-
 final_dataset[,"Margin.Changing"]<-data.frame(t(data.frame(lapply(final_dataset$Margin,function(x) strsplit(x," ")[[1]][1]))))[,1]
-
 # Changing data type
-
 final_dataset$BR<-as.numeric(final_dataset$BR)
 final_dataset$Runs.Changing<-as.numeric(as.character(final_dataset$Runs.Changing))
 final_dataset$Margin.Changing<-as.numeric(as.character(final_dataset$Margin.Changing))
-
 ####################################################################################################################
-
-# Applying the finisher criteria
 final<-data.frame()
-
 for(i in 1:length(unique(final_dataset$Player.Name)))
 {
      print(unique(final_dataset$Player.Name)[i])
-        
      # Subetting the corresponding player's data set
-     
      corres_player<-final_dataset[which(final_dataset$Player.Name==unique(final_dataset$Player.Name)[i]),]
-     
-     # Applying the finisher criteria
-     
-     # Criteria Runs>=30, Margin = 6, BR = 30 (5 overs), Carried.Status = Not-out, Result = won, 2nd batting
      if(nrow(corres_player)>50)
      {
-             if(nrow(corres_player[which(corres_player$Result=="won" & corres_player$Bat=="2nd"),])>25)
-             {
+       if(nrow(corres_player[which(corres_player$Result=="won" & corres_player$Bat=="2nd"),])>25)
+       {
      temp<-data.frame(unique(final_dataset$Player.Name)[i],fscore=as.numeric((nrow(corres_player[which(corres_player$Runs.Changing>=30 & corres_player$Margin.Changing<=6 & corres_player$BR<=30 & corres_player$Carried.Bat.Stauts=="Not-out" & corres_player$Result=="won" & corres_player$Bat=="2nd"),])/nrow(corres_player[which(corres_player$Result=="won" & corres_player$Bat=="2nd"),]))*100))
      colnames(temp)<-c("Player.Name","Finisher.Score")
      final<-rbind(final,temp)
-             }
+       }
      }    
 }
 # Removing 0's in the final dataset
-
 final<-final[-which(final[,2]<=0),]
-
 write.csv(final,"E:/Cricinfo/finisher_score_denom_change.csv",row.names = FALSE)
-
-# write.csv(corres_player,"E:/Cricinfo/player.csv",row.names = FALSE)
